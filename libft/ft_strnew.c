864c2753e1561dc451d43bd32446b23b6c401340
Silva Tross <stross@ox-l4.21-school.ru>
2019-09-11 15:25:45 +0300

    strnew update

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: stross <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 19:16:53 by stross            #+#    #+#             */
-/*   Updated: 2019/09/10 14:49:57 by stross           ###   ########.fr       */
+/*   Updated: 2019/09/11 15:25:07 by stross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 
 char	*ft_strnew(size_t size)
 {
-	void	*p;
+	void			*p;
+	unsigned long	check;
 
-	if (size + 1 > SIZE_MAX)
-		return (NULL);
-	p = malloc(size + 1);
-	if (p)
+	check = (unsigned long)size;
+	if (check + 1 <= SIZE_MAX)
 	{
-		p = (char*)ft_memset(p, '\0', size + 1);
-		return (p);
+		p = malloc(size + 1);
+		if (p)
+		{
+			p = (char*)ft_memset(p, '\0', size + 1);
+			return (p);
+		}
+		else
+			return (NULL);
 	}
-	else
-		return (NULL);
+	return (NULL);
 }
