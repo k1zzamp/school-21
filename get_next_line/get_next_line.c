a4e2446f3a24d839b75bbdeb00eed5560420a09e
Silva Tross <stross@ox-l4.21-school.ru>
2019-09-13 11:36:48 +0300

    gnl v2

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,82 +6,51 @@
 /*   By: stross <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 19:43:56 by stross            #+#    #+#             */
-/*   Updated: 2019/09/12 15:53:50 by stross           ###   ########.fr       */
+/*   Updated: 2019/09/13 11:35:18 by stross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char		*fill_array(t_list *list, char *arr)
+static t_list		*get_input(const int fd, t_list *list)
 {
-	char	*temp;
-	char	*ret;
-
-	ret = arr;
-	while (list != NULL)
-	{
-		temp = (char *) list->content;
-		while (*temp)
-			*arr++ = *temp++;
-		list = list->next;
-	}
-	return (ret);
-}
-
-static char		*get_arr_size(t_list *list)
-{
-	char	*arr;
-	char	*temp;
-	size_t	len;
-
-	len = 0;
-	while (list != NULL)
-	{
-		temp = (char *) list->content;
-		while (*temp)
-		{
-			len++;
-			temp++;
-		}
-		list = list->next;
-	}
-	arr = (char *) ft_memalloc((len + 1) * sizeof(char));
-	if (arr == NULL)
-		return (NULL);
-	return (arr);
-}
-
-static t_list	*get_input(const int fd, t_list *list)
-{
-	char		*buff[BUFF_SIZE]; // + 1 with no com
+	char		buff[BUFF_SIZE + 1];
+	char		*pta;
 	ssize_t		nbread;
+	char		end;
 
-	while ((nbread = read(fd, (void *) buff, (size_t) BUFF_SIZE) != 0))
+	end = '\0';
+	pta = buff;
+	while ((nbread = read(fd, (void *)buff, (size_t)BUFF_SIZE) != 0))
 	{
 		if (nbread == -1)
-			return (-1);
-//		buff[nbread] = '\0'; 
-		ft_lst_push_b(&list, (void *) buff, (size_t) BUFF_SIZE);
+			return (NULL);
+		buff[nbread] = '\0';
+		while (*pta)
+		{
+			ft_lst_push_b(&list, (void *)pta, 1);
+			pta++;
+		}
 	}
+	ft_lst_push_b(&list, (void *) &end, 1);
 	return (list);
 }
 
-int 			get_next_line(const int fd, char **line)
+int 				get_next_line(const int fd, char **line)
 {
-	t_list		*list;
-	static char	*arr;
+	static t_list	*list;
 
 	list = get_input(fd, list);
-	arr = get_arr_size(list);
-	arr = fill_array(list_arr);
-	while (*arr != EOF)
+	while (list->next)
 	{
-		if (*arr == NL)
+		if (*((char)(list->content)) == NL)
 		{
-			arr++;
-			return (1);
+			list = list->next;
+			return (0);
 		}
-		ft_putchar(*arr);
+		if (*((char)(list->content)) == END)
+			return (1);
+		list = list->next;
 	}
-	return (0);
+	return (-1);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: stross <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 19:44:48 by stross            #+#    #+#             */
-/*   Updated: 2019/09/12 15:53:52 by stross           ###   ########.fr       */
+/*   Updated: 2019/09/13 11:36:21 by stross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 
 # define BUFF_SIZE 4
 # define NL '\n'
+# define END '\0'
 
-# include "../libft/libft.h"
+# include "libft.h"
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <fcntl.h>
@@ -25,5 +26,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+
+int get_next_line(const int fd, char **line);
 
 #endif
